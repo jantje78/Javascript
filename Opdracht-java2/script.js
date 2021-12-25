7cd7f2683831516e56eb558bc605aa62f23bcc9d
Jan van der Steege <95129505+jantje78@users.noreply.github.com>
2021-12-25 16:55:00 +0100

    Add files via upload

---
+++ Opdracht-java2/script.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy students")
+// this is a large sum
+/*var multipliciation = 1230941 * 1823794;
+console.log(multipliciation)
+
+var division = 637263 / 54;
+console.log(division)
+*/
