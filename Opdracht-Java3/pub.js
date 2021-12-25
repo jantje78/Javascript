4f32249b0f688d56ceedee09bca7a147cd78b6a8
Jan van der Steege <95129505+jantje78@users.noreply.github.com>
2021-12-25 20:40:19 +0100

    Add files via upload

---
+++ Opdracht-Java3/pub.js
@@ -0,0 +1,26 @@
+const Age = 17;
+
+if (Age >= 18) {
+    console.log("allowed in");
+}
+if (Age < 18) {
+    console.log("not allowed in");
+
+}
+
+const isFemale = (false);
+if (isFemale) {
+    console.log("allowed in")
+} else {
+    console.log("not allowed in");
+}
+
+const driverStatus = 'designated';
+if (driverStatus == 'designated') {
+    console.log("allowed to drive");
+
+} else {
+    console.log("not allowed to drive");
+}
+
+
