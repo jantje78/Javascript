a999139e9c5f6de66ba922d5de35e2f962cca30b
Jan van der Steege <95129505+jantje78@users.noreply.github.com>
2021-12-25 13:29:37 +0100

    Add files via upload

---
+++ Opdracht_Java1/script.js
@@ -0,0 +1,26 @@
+
+let name = "jan";
+console.log(name)
+
+var numbers = 4 + 4
+console.log(numbers)
+
+var number = "4" + "4"
+console.log(number)
+
+var subtraction = 4 - 4
+console.log(subtraction)
+
+var multi = 4 * 4
+console.log(multi)
+
+var division = 16 / 4
+console.log(division)
+
+var modulus = 10 % 100
+console.log(modulus)
+
+var age = 48
+console.log(age)
+
+
