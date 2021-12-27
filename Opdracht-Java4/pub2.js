f2725ecf4ec57212cea9e92efca1748b87a94573
Jan van der Steege <95129505+jantje78@users.noreply.github.com>
2021-12-27 10:52:57 +0100

    Add files via upload

---
+++ Opdracht-Java4/pub2.js
@@ -0,0 +1,50 @@
+const Age = 25;
+
+if (Age >= 18) {
+    console.log("allowed in");
+}
+if (Age < 18) {
+    console.log("not allowed in");
+
+}
+
+const isFemale = (false);
+if (isFemale) {
+    console.log("allowed in");
+} else {
+    console.log("not allowed in");
+}
+
+const driverStatus = 'designated';
+if (driverStatus == 'designated') {
+    console.log("allowed to drive");
+
+} else {
+    console.log("not allowed to drive");
+}
+
+if (Age >= 18 && Age <= 25) {
+    console.log("You get 50% discount!!");
+} else {
+    console.log("You pay the full price!");
+}
+
+const firstname = ('Bram');
+
+if (firstname == 'Sarah' || 'Bram') {
+    console.log("You get free beers!!")
+}
+
+const totalAmount = 26
+if (totalAmount > 100) {
+    console.log("You get free champagne!");
+}
+
+else if (totalAmount > 50) {
+    console.log("You get free nacho's");
+}
+
+else if (totalAmount > 25) {
+    console.log("You get free veggieballs!!");
+}
+
