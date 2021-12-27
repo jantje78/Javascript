83269bbc82dcfa06f3adf423762fbae0a398841a
Jan van der Steege <95129505+jantje78@users.noreply.github.com>
2021-12-27 15:00:21 +0100

    Add files via upload

---
+++ Opdracht-Java5/objects.js
@@ -0,0 +1,40 @@
+/*
+
+let person = {
+    name: 'jan',
+    Age: 43
+}
+let evaluations = [7, 10, 9];
+
+let selectedColours = ["green", "blue", "red"];
+selectedColours[3] = "yellow"
+selectedColours.push("black")
+selectedColours.push(5)
+selectedColours.push({ Greeting: "Hi, I am an object" })
+
+
+
+
+console.log(person);
+
+console.log(person.name);
+
+console.log(person.Age);
+
+console.log(evaluations);
+
+console.log(selectedColours);
+
+console.log(selectedColours.length);
+console.log(selectedColours[selectedColours.length - 3]);
+
+console.log(selectedColours[0]);
+
+console.log(selectedColours);
+
+console.log(selectedColours[selectedColours.length - 1]);
+
+
+
+*/
+
