1e64ce1fa97cfa69df604febb80c295aaa84df2a
Jan van der Steege <95129505+jantje78@users.noreply.github.com>
2021-12-29 11:33:49 +0100

    Add files via upload

---
+++ Opdracht-Java6/script.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+    console.log("Maak het deeg");
+    console.log("Maak de tomatensaus");
+    console.log("Snij de groentes");
+    console.log("Bak de pizza");
+
+};
+
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log("Bereid de vis")
+    console.log("Kook de rijst")
+    console.log("Snij de groentes")
+    console.log("Rol de vis in de rijst")
+
+};
+makeSushi();
+makeSushi();
+makeSushi();
