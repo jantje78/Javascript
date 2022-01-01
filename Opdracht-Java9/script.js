10349016ddaa004cac500cbbde7a775de4c65795
Jan van der Steege <95129505+jantje78@users.noreply.github.com>
2022-01-01 12:06:10 +0100

    Add files via upload

---
+++ Opdracht-Java9/script.js
@@ -0,0 +1,34 @@
+
+
+function calculation(number1, number2) {
+    const squared1 = number1 * number1;
+    const squared2 = number2 * number2;
+    const total = squared1 + squared2;
+    const sum = total * total;
+
+    return sum;
+}
+console.log(calculation(2, 2))
+
+const multiplie = function (number1, number2) {
+    const squared1 = number1 * number1;
+    const squared2 = number2 * number2;
+    const total = squared1 + squared2;
+    const sum = total * total;
+
+    return sum;
+};
+
+console.log(multiplie(2, 2))
+
+
+const squaring = (number1, number2) => {
+    const squared1 = number1 * number1;
+    const squared2 = number2 * number2;
+    const total = squared1 + squared2;
+    const sum = total * total;
+
+    return sum;
+};
+
+console.log(squaring(2, 2))
